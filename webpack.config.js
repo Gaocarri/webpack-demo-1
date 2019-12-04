73fe04413fd16b850328070c94eef5a82685b513
GaoCarri <gaocarri@163.com>
2019-12-04 18:24:25 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
         filename: '[name].[contenthash].js'
-    }
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: '高正的网页',
+        template: 'src/assets/index.html'
+    })],
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        }, ],
+    },
 };
